643f5a83cc7908838358c633a13390345f46674e
maxay <Maxaytt@gmail.com>
2023-07-09 22:14:52 +0200

    refactor(configuration): remove unnecessary usings and shortened ConfigureServices

--- Pakosti.Api/Program.cs
+++ Pakosti.Api/Program.cs
@@ -1,16 +1,6 @@
-using System.Reflection;
-using System.Text;
-using Microsoft.AspNetCore.Authentication.JwtBearer;
-using Microsoft.AspNetCore.Authorization;
-using Microsoft.AspNetCore.Identity;
-using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
-using Microsoft.IdentityModel.Tokens;
-using Microsoft.OpenApi.Models;
+using Pakosti.Api.Extensions;
 using Pakosti.Application;
-using Pakosti.Application.Common.Mappings;
-using Pakosti.Domain.Entities;
 using Pakosti.Infrastructure.Persistence;
-using Pakosti.Api.Services.Identity;
 
 namespace Pakosti.Api;
 
@@ -38,104 +28,25 @@ public class Startup
             .AllowAnyHeader()
             .AllowAnyMethod());
 
-
         if (environment.IsDevelopment())
         {
             app.UseSwagger();
             app.UseSwaggerUI();
         }
 
-        
         app.UseAuthentication();
 
         app.UseRouting();
-        
+
         app.UseAuthorization();
         app.UseEndpoints(builder => builder.MapControllers());
     }
 
     public void ConfigureServices(IServiceCollection services)
     {
-        services.AddControllers();
-
         services
             .AddPersistence(_configuration)
-            .AddApplication();
-
-        services.AddAutoMapper(config =>
-        {
-            config.AddProfile(new AssemblyMappingProfile(Assembly.GetExecutingAssembly()));
-            config.AddProfile(new AssemblyMappingProfile(typeof(PakostiDbContext).Assembly));
-        });
-        services.AddSwaggerGen(opt =>
-        {
-            opt.SwaggerDoc("v1", new OpenApiInfo { Title = "Pakosti", Version = "v1" });
-            opt.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
-            {
-                Name = "Authorization",
-                Type = SecuritySchemeType.Http,
-                Scheme = "Bearer",
-                In = ParameterLocation.Header,
-                Description = "Please enter a valid Token.",
-                BearerFormat = "JWT"
-            });
-            opt.AddSecurityRequirement(new OpenApiSecurityRequirement
-            {
-                {
-                    new OpenApiSecurityScheme
-                    {
-                        Reference = new OpenApiReference
-                        {
-                            Type = ReferenceType.SecurityScheme,
-                            Id = "Bearer"
-                        }
-                    },
-                    Array.Empty<string>()
-                }
-            });
-        });
-
-        services.AddScoped<ITokenService, TokenService>();
-        services.AddAuthentication(cfg =>
-            {
-                cfg.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
-                cfg.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
-            })
-            .AddJwtBearer(options =>
-            {
-                options.TokenValidationParameters = new TokenValidationParameters
-                {
-                    ValidateIssuer = false,
-                    ValidateAudience = false,
-                    ValidateLifetime = true,
-                    ValidateIssuerSigningKey = true,
-                    ValidIssuer = _configuration["Jwt:Issuer"]!,
-                    ValidAudience = _configuration["Jwt:Audience"]!,
-                    IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_configuration["Jwt:Secret"]!))
-                };
-            });
-
-        services.AddAuthorization(cfg =>
-        {
-            cfg.DefaultPolicy =
-                new AuthorizationPolicyBuilder(JwtBearerDefaults.AuthenticationScheme)
-                    .RequireAuthenticatedUser()
-                    .Build();
-            cfg.AddPolicy("AdministratorOnly", policy =>
-                policy.RequireRole("Administrator"));
-        });
-
-        services.AddIdentity<AppUser, IdentityRole<Guid>>(options =>
-            {
-                options.Password.RequireNonAlphanumeric = false;
-            })
-            .AddEntityFrameworkStores<PakostiDbContext>()
-            .AddUserStore<UserStore<AppUser, IdentityRole<Guid>, PakostiDbContext, Guid>>()
-            .AddRoleStore<RoleStore<IdentityRole<Guid>, PakostiDbContext, Guid>>()
-            .AddUserManager<UserManager<AppUser>>()
-            .AddRoleManager<RoleManager<IdentityRole<Guid>>>()
-            .AddSignInManager<SignInManager<AppUser>>()
-            .AddDefaultTokenProviders()
-            .AddRoles<IdentityRole<Guid>>();
+            .AddApplication()
+            .ConfigureApiServices(_configuration);
     }
 }
