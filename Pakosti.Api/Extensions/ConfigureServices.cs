0258b028e65323f1da37bd3014ed39961a650f7a
maxay <Maxaytt@gmail.com>
2023-07-09 22:09:33 +0200

    refactor(configuration): add ConfigureServices.cs for api configurations

---
+++ Pakosti.Api/Extensions/ConfigureServices.cs
@@ -0,0 +1,32 @@
+using System.Reflection;
+using Pakosti.Api.Services.Identity;
+using Pakosti.Application.Common.Mappings;
+using Pakosti.Infrastructure.Persistence;
+
+namespace Pakosti.Api.Extensions;
+
+public static class ConfigureServices
+{
+    public static IServiceCollection ConfigureApiServices(
+        this IServiceCollection services, IConfiguration configuration)
+    {
+        services
+            .ConfigureSwagger()
+            .ConfigureAuthentication(configuration)
+            .ConfigureAuthorization()
+            .ConfigureIdentity();
+        
+        services.AddControllers();
+        services.AddEndpointsApiExplorer();
+        
+        services.AddAutoMapper(config =>
+        {
+            config.AddProfile(new AssemblyMappingProfile(Assembly.GetExecutingAssembly()));
+            config.AddProfile(new AssemblyMappingProfile(typeof(PakostiDbContext).Assembly));
+        });
+
+        services.AddScoped<ITokenService, TokenService>();
+        
+        return services;
+    }
+}
