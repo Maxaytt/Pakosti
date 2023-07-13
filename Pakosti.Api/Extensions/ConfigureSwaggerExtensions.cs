b9b9aeab007ff687ef45401a5ce51fd2a2399390
maxay <Maxaytt@gmail.com>
2023-07-13 14:20:16 +0200

    fix: add custom schema ID's

--- Pakosti.Api/Extensions/ConfigureSwaggerExtensions.cs
+++ Pakosti.Api/Extensions/ConfigureSwaggerExtensions.cs
@@ -4,8 +4,9 @@ namespace Pakosti.Api.Extensions;
 
 public static class ConfigureSwaggerExtensions
 {
+    private const string IgnoredNamespaceIdentifiersKey = "Swagger:IgnoredNamespaceIdentifiers";
     public static IServiceCollection ConfigureSwagger(
-        this IServiceCollection services) => services.AddSwaggerGen(options =>
+        this IServiceCollection services, IConfiguration configuration) => services.AddSwaggerGen(options =>
     {
         options.SwaggerDoc("v1", new OpenApiInfo { Title = "Pakosti", Version = "v1" });
         options.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
@@ -16,6 +17,24 @@ public static class ConfigureSwaggerExtensions
             In = ParameterLocation.Header,
             Description = "Please enter a valid Token.",
             BearerFormat = "JWT"
+        });
+        options.CustomSchemaIds(type =>
+        {
+            // Ignored parts of namespaces, generally CQRS-conventional names,
+            // such as 'Queries' and 'Commands'. These are skipped when generating
+            // Swagger names for the public DTOs.
+            var ignoredIdentifiers = configuration
+                .GetSection(IgnoredNamespaceIdentifiersKey)
+                .Get<HashSet<string>>()!;
+
+            // Generates unique and user-friendly names for CQRS entities.
+            // For example, 'Features.Accounts.Commands.Create.Command' gets turned into 'AccountsCreateCommand'.
+            var lastNames = type.FullName!.Split('.', '+')
+                .Where(identifier => !ignoredIdentifiers.Contains(identifier))
+                .TakeLast(3);
+
+            return string.Join(string.Empty, lastNames);
+
         });
         options.AddSecurityRequirement(new OpenApiSecurityRequirement
         {
