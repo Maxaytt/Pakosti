ea64674c1073de16055370e39f49ce6a616e368b
Mykola Piskunov <118397025+PiskunovMykola@users.noreply.github.com>
2023-07-24 14:07:32 +0200

    fix(validations): change validation authenticate

--- Pakosti.Application/Features/Identities/Commands/Authenticate.cs
+++ Pakosti.Application/Features/Identities/Commands/Authenticate.cs
@@ -25,12 +25,11 @@ public static class Authenticate
 
             RuleFor(x => x.Password)
                 .NotEmpty().WithMessage("Password is required")
-                .MinimumLength(6).WithMessage("Password must be at least 6 characters long")
-                .MaximumLength(50).WithMessage("Email must not exceed 50 characters")
+                .MinimumLength(8).WithMessage("Password must be at least 8 characters long")
                 .Matches("[A-Z]").WithMessage("Password must contain at least one uppercase letter")
                 .Matches("[a-z]").WithMessage("Password must contain at least one lowercase letter")
                 .Matches("[0-9]").WithMessage("Password must contain at least one digit")
-                .Matches("[!@#$%^&*]").WithMessage("Password must contain at least one special character (!@#$%^&*)");;
+                .Matches("[!@#$%\\^&*()\\[\\]{};':\",.<>\\/\\-=_+]").WithMessage("Password must contain at least one special character");
         }
     }
     
