6e30aee1494aca97291de85f580b5745032db3a8
PiskunovMykola <118397025+PiskunovMykola@users.noreply.github.com>
2023-07-24 12:34:16 +0200

    feat(validators): add command validation in authenticate

--- Pakosti.Application/Features/Identities/Commands/Authenticate.cs
+++ Pakosti.Application/Features/Identities/Commands/Authenticate.cs
@@ -1,3 +1,4 @@
+using FluentValidation;
 using MediatR;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.Extensions.Configuration;
@@ -13,6 +14,26 @@ public static class Authenticate
 {
     public sealed record Command(string Email, string Password) : IRequest<Response>;
 
+    public class Validator : AbstractValidator<Command>
+    {
+        public Validator()
+        {
+            RuleFor(x => x.Email)
+                .NotEmpty().WithMessage("Email is required")
+                .EmailAddress().WithMessage("Invalid email address")
+                .MaximumLength(50).WithMessage("Email must not exceed 50 characters");;
+
+            RuleFor(x => x.Password)
+                .NotEmpty().WithMessage("Password is required")
+                .MinimumLength(6).WithMessage("Password must be at least 6 characters long")
+                .MaximumLength(50).WithMessage("Email must not exceed 50 characters")
+                .Matches("[A-Z]").WithMessage("Password must contain at least one uppercase letter")
+                .Matches("[a-z]").WithMessage("Password must contain at least one lowercase letter")
+                .Matches("[0-9]").WithMessage("Password must contain at least one digit")
+                .Matches("[!@#$%^&*]").WithMessage("Password must contain at least one special character (!@#$%^&*)");;
+        }
+    }
+    
     public sealed class Handler : IRequestHandler<Command, Response>
     {
         private readonly IIdentityRepository _repository;
