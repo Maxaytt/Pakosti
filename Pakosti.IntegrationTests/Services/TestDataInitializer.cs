75aa4f4e6c9b7dbc93d95ecd7905b5a70a58d640
maxay <Maxaytt@gmail.com>
2023-08-04 14:59:20 +0200

    feat(tests): add createProduct method to initializer

--- Pakosti.IntegrationTests/Services/TestDataInitializer.cs
+++ Pakosti.IntegrationTests/Services/TestDataInitializer.cs
@@ -3,6 +3,7 @@ using System.Net.Http.Json;
 using AutoFixture;
 using Pakosti.Application.Features.Categories.Commands;
 using Pakosti.Application.Features.Identities.Commands;
+using Pakosti.Application.Features.Products.Commands;
 using Pakosti.IntegrationTests.Setups;
 
 namespace Pakosti.IntegrationTests.Services;
@@ -31,6 +32,15 @@ public class TestDataInitializer
         var request = new CreateCategory.Command(dto.parentId, dto.name);
         var response = await client.PostAsJsonAsync("/api/category", request);
         var responseData = await response.Content.ReadFromJsonAsync<CreateCategory.Response>();
+        return responseData!.Id;
+    }
+    
+    public static async Task<Guid> CreateProduct(HttpClient client, 
+        (Guid categoryId, string name, string description) dto)
+    {
+        var request = new CreateProduct.Dto(dto.categoryId, dto.name, dto.description);
+        var response = await client.PostAsJsonAsync("/api/product", request);
+        var responseData = await response.Content.ReadFromJsonAsync<CreateProduct.Response>();
         return responseData!.Id;
     }
 }
