c1c8eb23617e82fd224f8cd6b39905fd4ca36d04
PiskunovMykola <118397025+PiskunovMykola@users.noreply.github.com>
2023-08-12 12:57:22 +0200

    fix(exception): delete ErrorsDictionary

--- Pakosti.Application/Common/Exceptions/BadRequestException.cs
+++ Pakosti.Application/Common/Exceptions/BadRequestException.cs
@@ -3,8 +3,6 @@ namespace Pakosti.Application.Common.Exceptions;
 public class BadRequestException : Exception
 {
     public string Description { get; }
-    public IReadOnlyDictionary<string, string[]> ErrorsDictionary { get; set; }
-
 
     public BadRequestException(string description)
         : base(description)
