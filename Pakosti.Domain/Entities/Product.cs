8964cd6091a5dec8b21889193013a841deed1d69
maxay <Maxaytt@gmail.com>
2023-08-25 12:25:35 +0200

    feat(models): add price to product model

--- Pakosti.Domain/Entities/Product.cs
+++ Pakosti.Domain/Entities/Product.cs
@@ -3,9 +3,9 @@ namespace Pakosti.Domain.Entities;
 public class Product
 {
     public Guid Id { get; set; }
-    public Guid UserId { get; set; }
     public Guid? CategoryId { get; set; }
 
+    public Price Price { get; set; } = null!; 
     public string Name { get; set; } = null!;
     public string Description { get; set; } = null!;
     public DateTimeOffset CreationDate { get; set; }
