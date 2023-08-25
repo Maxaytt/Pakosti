6bd9213dfcdb5ddff2edd96c3c8af39fce25df5b
maxay <Maxaytt@gmail.com>
2023-08-25 12:34:50 +0200

    feat(exceptions): add internal server exception

---
+++ Pakosti.Application/Exceptions/InternalServerException.cs
@@ -0,0 +1,14 @@
+namespace Pakosti.Application.Exceptions;
+
+public class InternalServerException : ApplicationException
+{
+    public string EntityName { get; }
+    public object EntityId { get; }
+
+    public InternalServerException(string entityName, object entityId, string massage)
+        : base(massage)
+    {
+        EntityName = entityName;
+        EntityId = entityId;
+    }
+}
