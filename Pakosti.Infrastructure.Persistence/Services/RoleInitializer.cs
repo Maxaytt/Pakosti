fb2dfa04838578a17bbce083a7a02eaf5bf91aa9
maxaytt <Maxaytt@gmail.com>
2023-09-15 20:01:39 +0200

    refactor: remove unnecessary using, merge route and http action attribute

--- Pakosti.Infrastructure.Persistence/Services/RoleInitializer.cs
+++ Pakosti.Infrastructure.Persistence/Services/RoleInitializer.cs
@@ -2,7 +2,6 @@ using Microsoft.AspNetCore.Identity;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Pakosti.Domain.Constants;
-using Pakosti.Domain.Entities;
 
 namespace Pakosti.Infrastructure.Persistence.Services;
 
