07c2a0f37061811c2ab736316194e118f8b9b924
maxaytt <Maxaytt@gmail.com>
2023-10-02 13:09:51 +0200

    fix(handlers): update product less effective way, but more readable

--- Pakosti.Application/Features/Administrator/Products/Commands/UpdateProduct.cs
+++ Pakosti.Application/Features/Administrator/Products/Commands/UpdateProduct.cs
@@ -45,14 +45,9 @@ public static class UpdateProduct
                 if (category == null) throw new NotFoundException(nameof(Category), request.CategoryId);
                 product.CategoryId = request.CategoryId;
             }
-            if (request.Name != null)
-            {
-                product.Name = request.Name;
-            }
-            if (request.Description != null)
-            {
-                product.Description = request.Description;
-            }
+            
+            product.Name = request.Name ?? product.Name;
+            product.Description = request.Description ?? product.Description;
             product.EditionDate = DateTimeOffset.UtcNow;
             
             await _context.SaveChangesAsync(cancellationToken);
