6214f8c77030fa4f511f6cc5a440a34301dfe9db
maxaytt <Maxaytt@gmail.com>
2023-10-02 12:13:47 +0200

    feat: add role and area constants

---
+++ Pakosti.Domain/Constants/AreaConstants.cs
@@ -0,0 +1,9 @@
+namespace Pakosti.Domain.Constants;
+
+public static class AreaConstants
+{
+    public const string Administrator = "Admin";
+    public const string SuperAdministrator = "Super";
+    public const string Consumer = "Consumer";
+    public const string Guest = "Guest";
+}
--- Pakosti.Domain/Constants/RoleConstants.cs
+++ Pakosti.Domain/Constants/RoleConstants.cs
@@ -2,6 +2,7 @@ namespace Pakosti.Domain.Constants;
 
 public static class RoleConstants
 {
+    public const string Guest = "Guest";
     public const string Consumer = "Consumer";
     public const string Administrator = "Administrator";
     public const string SuperAdministrator = "SuperAdministrator";
